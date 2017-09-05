446908344002b05229a08524971a03973769af40
siwix1 <stiantungewixoe@icloud.com>
2017-09-05 23:48:12 +1000

    New menuitems

---
+++ Models/HCPHistory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BirdieBook.Models
+{
+    public class HCPHistory
+    {
+        public string UserID { get; set; }
+        public DateTime HCPTime { get; set; }
+        public bool ManualUpdate { get; set; }
+        public decimal OldHandicap { get; set; }
+        public decimal NewHandicap { get; set; }
+
+
+    }
+}
